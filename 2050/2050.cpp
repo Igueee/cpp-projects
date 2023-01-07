079b0846887cbf84eb2832a2bf47158598b7efed
Igue_ <68257745+Igueee@users.noreply.github.com>
2023-01-06 21:22:00 -0300

    Add files via upload

---
+++ 2050/2050.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+int sub = 2050;
+int year = 0;
+std::cout << "in what year were you born? \n";
+std::cin >> year;
+int age = sub - year;
+std::cout << "your age in 2050 will be: \n";
+std::cout << age << "\n";
+}

19fde1b244898406afa8144e14fa759c03b87bc7
Igue_ <68257745+Igueee@users.noreply.github.com>
2023-01-26 21:30:06 +0000

    nvm

---
+++ useraccounts[W.I.P]/useraccounts.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main(){
+    int act = 0;
+    std::cout << "which one you want to do? \n 0-Log in into an existing accout \n 1-Resgister a new accont";
+    std::cin >> act;
+    if(act == 0){
+        logacc();
+    }else{
+        regacc();
+    }
+}
+
+int logacc(){
+
+}
+
+int regacc(){
+
+}
